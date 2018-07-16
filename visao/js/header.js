d7673de2d8a6a2103466a1eb092ba4c9b591ee0c
luizsergiorf <luizsergiorferreira@hotmail.com>
2018-07-16 17:07:36 -0300

    Javascript fix

--- visao/js/header.js
+++ visao/js/header.js
@@ -1,11 +1,7 @@
 'use strict';
 
 (function () {
-  var settings = {
-    shrinkHeaderAfter: 50
-  };
 
-  var mainHeader = document.querySelector('.main-header');
   var btnMenu = document.querySelector('.btn-menu');
   var elNavigation = document.querySelector('.navigation');
 
@@ -16,15 +12,5 @@
     elNavigation.classList.toggle('open');
   };
 
-  window.onscroll = function () {
-    'use strict';
-
-    console.log(document.documentElement.scrollTop);
-    if (document.body.scrollTop > settings.shrinkHeaderAfter || document.documentElement > settings.shrinkHeaderAfter) {
-      mainHeader.classList.add('shrink');
-    } else {
-      mainHeader.classList.remove('shrink');
-    }
-  };
 })();
-//# sourceMappingURL=header.js.map
+
